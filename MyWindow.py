bd6ac2130cd8503248af9b44931783fce581c049
David Tooley <David.Tooley@live.com>
2014-09-04 00:20:02 -0400

    about to make dramatic changes in the hopes of seeing a circle

--- MyWindow.py
+++ MyWindow.py
@@ -20,15 +20,45 @@ class ColoredBall(QWidget):
         self.CreatePath()
 
         self.PenColor = QColor("black")
-        self.FillColor = QColor(color)
+        self.FillColor1 = QColor(color)
+        self.FillColor2 = QColor("white")
+        self.rotationAngle = 0
         self.PenWidth = 1
         self.setBackgroundRole(QPalette.Base)
+
+    def minimumSizeHint(self):
+        return QSize(50, 50)
+
+    def sizeHint(self):
+        return QSize(100, 100)
+
+    def setFillRule(self, rule):
+        self.path.setFillRule(rule)
+        self.update()
+
+    def setFillGradient(self, color1, color2):
+        self.FillColor1 = color1
+        self.FillColor2 = color2
+        self.update()
+
+    def setPenWidth(self, width):
+        self.penWidth = width
+        self.update()
+
+    def setPenColor(self, color):
+        self.penColor = color
+        self.update()
+
+    def setRotationAngle(self, degrees):
+        self.rotationAngle = degrees
+        self.update()
 
 
     def CreatePath(self):
         self.path = QPainterPath()
-        self.path.arcMoveTo(self.yPos, self.xPos, 10.0, 10.0, 360.0)
+        self.path.arcMoveTo(self.yPos, self.xPos, 50.0, 60.0, 360.0)
         self.path.closeSubpath()
+        self.path.setFillRule(Qt.FillRule(Qt.WindingFill))
 
 
     def paintEvent(self, event):
@@ -36,31 +66,30 @@ class ColoredBall(QWidget):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.scale(self.width() / 100.0, self.height() / 100.0)
         painter.translate(50.0, 50.0)
-        painter.rotate(0)
+        painter.rotate(-self.rotationAngle)
         painter.translate(-50.0, -50.0)
 
         painter.setPen(
                 QPen(self.PenColor, self.PenWidth, Qt.SolidLine, Qt.RoundCap,
                         Qt.RoundJoin))
         gradient = QLinearGradient(0, 0, 0, 100)
-        gradient.setColorAt(0.0, self.FillColor)
-        gradient.setColorAt(1.0, self.FillColor)
+        gradient.setColorAt(0.0, self.FillColor1)
+        gradient.setColorAt(1.0, self.FillColor2)
         painter.setBrush(QBrush(gradient))
         painter.drawPath(self.path)
 
 
-class myWindow(QDialog):
+class myWindow(QWidget):
     def __init__(self):
         super(myWindow, self).__init__()
 
-        self.createMenu()
+
         self.createControl()
-        self.createSolver()
+        self.createPuzzle()
 
         mainLayout = QHBoxLayout()
-        mainLayout.setMenuBar(self.menuBar)
-        mainLayout.addWidget(self.controls)
-        mainLayout.addWidget(self.locations)
+        mainLayout.addLayout(self.Buttons)
+        mainLayout.addLayout(self.labels)
 
         self.btn_RotateCCL.clicked.connect(self.rotateCCL)
         self.btn_RotateCCR.clicked.connect(self.rotateCCR)
@@ -91,33 +120,32 @@ class myWindow(QDialog):
     def Solve(self):
         pass
 
-    def createSolver(self):
-
-        self.locations = QGroupBox()
-        labels = QGridLayout()
+    def createPuzzle(self):
 
 
-        self.labelNorth = ColoredBall("black",50,150)
-        self.labelSouth = QLabel("South")
-        self.labelEast = QLabel("East")
-        self.labelWest = QLabel("West")
+        self.labels = QGridLayout()
 
-        labels.addWidget(self.labelNorth, 1, 2)
-        labels.addWidget(self.labelWest, 2, 1)
-        labels.addWidget(self.labelEast, 2, 3)
-        labels.addWidget(self.labelSouth, 3, 2)
-        numCols = 4
-        numRows = 5
-        for i in range(numRows):
-            labels.setRowMinimumHeight(i, 50)
-        for j in range(numCols):
-            labels.setColumnMinimumWidth(j, 50)
 
-        self.locations.setLayout(labels)
+        self.labelNorth = ColoredBall("black",50.0,200.0)
+        self.labelSouth = ColoredBall("maroon",150.0,200.0)
+        self.labelEast = ColoredBall("green",100.0,150.0)
+        self.labelWest = ColoredBall("blue",100.0,250.0)
+
+        self.labels.addWidget(self.labelNorth, 1, 2)
+        self.labels.addWidget(self.labelWest, 2, 1)
+        self.labels.addWidget(self.labelEast, 2, 3)
+        self.labels.addWidget(self.labelSouth, 3, 2)
+#        numCols = 4
+#        numRows = 5
+#        for i in range(numRows):
+#            labels.setRowMinimumHeight(i, 50)
+#        for j in range(numCols):
+#            labels.setColumnMinimumWidth(j, 50)
+
+
 
     def createControl(self):
-        self.controls = QGroupBox()
-        Buttons = QGridLayout()
+        self.Buttons = QGridLayout()
 
         self.btn_RotateCL = QPushButton("Rotate Left Side Clockwise")
         self.btn_RotateCCL = QPushButton("Rotate Left Side Counter-Clockwise")
@@ -132,31 +160,22 @@ class myWindow(QDialog):
         buttonColWidth = 215
 
 
-        Buttons.addWidget(self.btn_RotateCL, 1, 0)
-        Buttons.addWidget(self.btn_RotateCCL, 1, 1)
-        Buttons.addWidget(self.btn_RotateCR, 2, 0)
-        Buttons.addWidget(self.btn_RotateCCR, 2, 1)
-        Buttons.addWidget(self.btn_Randomize, 4, 0)
-        Buttons.addWidget(self.btn_Solve, 4, 1)
+        self.Buttons.addWidget(self.btn_RotateCL, 1, 0)
+        self.Buttons.addWidget(self.btn_RotateCCL, 1, 1)
+        self.Buttons.addWidget(self.btn_RotateCR, 2, 0)
+        self.Buttons.addWidget(self.btn_RotateCCR, 2, 1)
+        self.Buttons.addWidget(self.btn_Randomize, 4, 0)
+        self.Buttons.addWidget(self.btn_Solve, 4, 1)
 
-        numRows=6 #runs through each row to ensure padding, also adds padding to top and bottom rows
+#        numRows=6 #runs through each row to ensure padding, also adds padding to top and bottom rows
+#
+#        for i in range(numRows):
+#            Buttons.setRowMinimumHeight(i,10)
+#
+#        Buttons.setColumnMinimumWidth(0, buttonColWidth)
+#        Buttons.setColumnMinimumWidth(1, buttonColWidth)
 
-        for i in range(numRows):
-            Buttons.setRowMinimumHeight(i,10)
 
-        Buttons.setColumnMinimumWidth(0, buttonColWidth)
-        Buttons.setColumnMinimumWidth(1, buttonColWidth)
-
-        self.controls.setLayout(Buttons)
-
-    def createMenu(self):
-        self.menuBar = QMenuBar()
-
-        self.fileMenu = QMenu("File", self)
-        self.exitAction = self.fileMenu.addAction("Exit")
-        self.menuBar.addMenu(self.fileMenu)
-
-        self.exitAction.triggered.connect(self.accept)
 
 
 if __name__ == '__main__':
@@ -164,6 +183,7 @@ if __name__ == '__main__':
 
 
     app = QApplication(sys.argv)
-    dialog = myWindow()
-    sys.exit(dialog.exec_())
+    myWindow = myWindow()
+    myWindow.show()
+    sys.exit(app.exec_())
 
